83b2ac7c658f30b6bf80a027d63d1c862c7346ce
selsibaie <s.elsibaie@gmail.com>
2017-08-22 15:31:31 -0400

    setup unit tests and core functions

--- g33standards.py
+++ g33standards.py
@@ -6,5 +6,13 @@ of MSRB g33 to various input types
 import decimal
 
 def apply_accrued_interest_rules(num):
-    c = decimal.Decimal(num).quantize(decimal.Decimal('.01'), rounding=decimal.ROUND_HALF_UP)
-    return str(c)
+    num = decimal.Decimal(str(num)).quantize(decimal.Decimal('.01'), rounding=decimal.ROUND_HALF_UP)
+    return str(num)
+
+def apply_dollar_price_rules(num):
+    num = decimal.Decimal(str(num)).quantize(decimal.Decimal('.001'), rounding=decimal.ROUND_DOWN)
+    return str(num)
+
+def apply_yield_rules(num):
+    num = decimal.Decimal(str(num)).quantize(decimal.Decimal('.001'), rounding=decimal.ROUND_HALF_UP)
+    return str(num)
--- unittest.py
+++ unittest.py
@@ -8,3 +8,15 @@ def test_accrued_interest():
     assert g33standards.apply_accrued_interest_rules(29384.2445089) == "29384.24"
     assert g33standards.apply_accrued_interest_rules(2.035) == "2.04"
     assert g33standards.apply_accrued_interest_rules(2.045) == "2.05"
+    assert g33standards.apply_accrued_interest_rules(-2.045) == "-2.05"
+    assert g33standards.apply_accrued_interest_rules(-2.035) == "-2.04"
+
+def test_dollar_price():
+    assert g33standards.apply_dollar_price_rules(4.1269834) == "4.126"
+    assert g33standards.apply_dollar_price_rules(-2.1535) == "-2.153"
+    assert g33standards.apply_dollar_price_rules(-2.1534) == "-2.153"
+
+def test_yield():
+    assert g33standards.apply_yield_rules(4.1269834) == "4.127"
+    assert g33standards.apply_yield_rules(-2.1535) == "-2.154"
+    assert g33standards.apply_yield_rules(-2.1534) == "-2.153"
