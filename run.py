01931f6c3477c50c6d59cfb68ca30b2f5ad58ee0
s <s@ss-MacBook.localdomain>
2017-08-27 20:19:08 -0400

    Added looping, corrected unit test

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# coding=utf-8
 '''
 summary
 '''
@@ -7,14 +9,18 @@ summary
 import sys
 import g33standards
 
-inputtype = input("Please enter a letter indicating if the user will input Accrued Interest, Dollar Price, or Yield (i.e., “A”, “D”, or “Y”): ")
-num = input("Please enter a number: ")
+while (True):
 
-if inputtype == "A":
-    print(g33standards.apply_accrued_interest_rules(num))
-elif inputtype == "D":
-    print(g33standards.apply_dollar_price_rules(num))
-elif inputtype == "Y":
-    print(g33standards.apply_yield_rules(num))
-else:
-    pass
+    inputtype = input("Please enter a letter indicating if the user will input Accrued Interest, Dollar Price, or Yield (i.e., “A”, “D”, or “Y”): ")
+    num = input("Please enter a number: ")
+
+    if inputtype == "A":
+        print(g33standards.apply_accrued_interest_rules(num))
+    elif inputtype == "D":
+        print(g33standards.apply_dollar_price_rules(num))
+    elif inputtype == "Y":
+        print(g33standards.apply_yield_rules(num))
+    else:
+        pass
+    if (input("Another? [Y/n]: ") == "n"):
+        break
